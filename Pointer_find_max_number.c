76a2e596e8ff08aa839b2f6c40a1654850d88e75
Fırat günay <127590689+ItsFirat@users.noreply.github.com>
2023-05-13 16:29:35 +0300

    Update Pointer_find_max_number.c

--- Pointer_find_max_number.c
+++ Pointer_find_max_number.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sum(int* , int*);
+int maximum(int* , int*);
 int main()
 {
-    int num1, num2, topla;
-    int *ptr1, *ptr2;
+    int num1, num2, max;
     printf("enter the numbers :");
     scanf("%d%d", &num1,&num2);
-    topla = sum(&num1 ,&num2);
-    printf("max : %d", topla);
+    max = maximum(&num1 ,&num2);
+    printf("max : %d", max);
     return 0;
 }
-int sum(int* a , int* b)
+int maximum(int* a , int* b)
 {
     int max;
     if(*a > *b){
