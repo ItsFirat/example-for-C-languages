066e9668c57c67da5d4acbdf296d326dc5e84a8d
Fırat günay <127590689+firatgunay@users.noreply.github.com>
2023-05-15 21:40:31 +0300

    Add files via upload

---
+++ lenghtOfString.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int lenghtOfString(char* );
+int main()
+{
+    int sonuc;
+    char str[25];
+    printf("enter the string :");
+    gets(str);
+    sonuc = lenghtOfString(str);
+    printf("lenght of the string : %d", sonuc);
+    return 0;
+}
+int lenghtOfString(char* ch){
+    int len = 0;
+    while(*ch !='\0'){
+        len ++;
+        ch++;
+    }
+    return len;
+
+}
